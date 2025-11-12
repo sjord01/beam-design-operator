bb713722e8af61c8a2962bc9bff4f89b67767ddc
Sam Ordonez <samsonordonez.ce@gmail.com>
2025-11-11 20:31:33 -0800

    adding deployment yaml files

---
+++ operator/src/main/java/sordonez/operator/OperatorApplication.java
@@ -0,0 +1,15 @@
+package sordonez.operator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperatorApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(OperatorApplication.class, args);
+    }
+
+}
---
+++ operator/src/test/java/sordonez/operator/OperatorApplicationTests.java
@@ -0,0 +1,15 @@
+package sordonez.operator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperatorApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
